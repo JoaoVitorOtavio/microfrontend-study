16865b80259e1f4ef9e2fa0167bef7184fa285f7
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-03-19 21:22:00 -0300

    Configurando webpack para dev

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,21 @@
+// necessario para mergear as configuracoes de dev com as configuracoes de common
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
