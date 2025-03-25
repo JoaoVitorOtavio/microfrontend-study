2a9c46d580412c963e1a897be03db92966fc01c5
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-03-24 21:09:50 -0300

    Movendo de lugar o htmlwebpackplugin

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 // necessario para mergear as configuracoes de dev com as configuracoes de common
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
@@ -21,9 +20,6 @@ const devConfig = {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
