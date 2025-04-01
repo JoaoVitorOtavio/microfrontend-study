4f7ce6dca821bfec11b326d81c045e7ca8b81156
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-04-01 20:36:59 -0300

    Reorganizando a estrutura do componente App para melhorar a legibilidade

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -15,14 +15,14 @@ const generateClassName = createGenerateClassName({
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <StylesProvider classNameGenerator={generateClassName}>
+      <StylesProvider classNameGenerator={generateClassName}>
+        <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </StylesProvider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
   );
 };
